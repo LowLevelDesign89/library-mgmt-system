7279f5eac3398843172607e1d99424fd36deaac8
tanayakarmakar <karmakar.tanaya@gmail.com>
2024-03-07 08:46:53 +0530

    library management system

---
+++ src/main/java/com/app/libmgmt/commons/AppConstants.java
@@ -0,0 +1,6 @@
+package com.app.libmgmt.commons;
+
+public class AppConstants {
+    public static final int MAX_BOOKS_ALLOWED_PER_MEMBER = 5;
+    public static final int MAX_TO_DAY_ALLOWED_TO_KEEP_BOOK = 10;
+}
